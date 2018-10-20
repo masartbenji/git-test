b4b4f0705d556bcd78fe54ea0f082703a61a2adb
Gwynbleidd96 <alessandro.pozzi72@gmail.com>
2018-10-20 11:34:52 +0200

    Creation de la classe UserController

---
+++ src/main/java/com/henallux/gittest/UserController.java
@@ -0,0 +1,15 @@
+package com.henallux.gittest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @RequestMapping(method = RequestMethod.GET)
+    public String home(Model model) {
+        return "user.jsp";
+    }
+}
